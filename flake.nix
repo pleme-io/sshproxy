82033a90721dec9a1ace088206a60d66226e802b
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:53 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Go library to proxy and intercept SSH connections";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          go
+          gopls
+          gotools
+        ];
+      };
+    });
+}
